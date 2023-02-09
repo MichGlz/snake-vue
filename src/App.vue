8710afb41d717fcf112c730d8a0ba666a56415fd
Mich Gonzalez <miguel.glz.da@gmail.com>
2023-02-09 15:55:04 +0100

    prompt name working and styled

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,62 @@
-<script setup>
-import GameBoard from "./components/GameBoard.vue";
-const FRAMES_PER_SECOND = 5;
-</script>
-
 <template>
-  <div class="game-wrapper">
-    <h2>Snake Game</h2>
-    <GameBoard :speed="FRAMES_PER_SECOND"></GameBoard>
+  <div class="game-wrapper" :class="{ dark: !isNameInput }">
+    <h2 v-if="isNameInput">Snake Game</h2>
+    <ModalName
+      v-if="!isNameInput"
+      :updateUser="updateUser"
+      @name-submited="updateUser"
+    />
+    <GameBoard v-else :speed="FRAMES_PER_SECOND" :userName="userName" />
   </div>
 </template>
+
+<script>
+import GameBoard from "./components/GameBoard.vue";
+import ModalName from "./components/ModalName.vue";
+export default {
+  components: {
+    GameBoard,
+    ModalName,
+  },
+  data() {
+    return {
+      FRAMES_PER_SECOND: 5,
+      isNameInput: false,
+      user: {},
+      scoreList: [],
+    };
+  },
+  methods: {
+    updateUser(val) {
+      this.user = val;
+      this.isNameInput = true;
+    },
+    updateScoreList(user) {
+      const list = JSON.parse(localStorage.getItem("scoreList"));
+      list.push(user);
+      list.sort((a, b) => {
+        if (a.score < b.score) {
+          return -1;
+        }
+        if (a.score > b.score) {
+          return 1;
+        }
+        return 0;
+      });
+      localStorage.setItem("scoreList", `${JSON.stringify(list)}`);
+      this.scoreList = [...list];
+    },
+  },
+  mounted() {
+    if (localStorage.getItem("scoreList")) {
+      this.scoreList = JSON.parse(localStorage.getItem("scoreList"));
+    } else {
+      let list = [];
+      localStorage.setItem("scoreList", `${JSON.stringify(list)}`);
+    }
+  },
+};
+</script>
 
 <style scoped>
 .logo {
---
+++ src/components/ModalName.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="modal-wrapper">
+    <div class="modal">
+      <form class="form-modal" @submit.prevent="onsubmit">
+        <p>Before starting the game, enter your name please.</p>
+        <label class="label-modal">
+          name:
+          <input
+            type="text"
+            class="input-modal"
+            id="userName"
+            placeholder="_"
+            required
+            v-model="userName"
+          />
+        </label>
+        <button
+          class="btn-modal"
+          type="submit"
+          value="Submit"
+          @click="hadleClick"
+        >
+          submit
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    gameStar: Function,
+    updateUser: Function,
+  },
+  data() {
+    return {
+      userName: "",
+    };
+  },
+  methods: {
+    onsubmit() {
+      let user = {
+        name: this.userName,
+        score: 0,
+      };
+      setTimeout(() => {
+        this.$emit("name-submited", user);
+      }, 250);
+    },
+  },
+  computed() {},
+};
+</script>
+
+<style scoped>
+.modal-wrapper {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 10;
+}
+
+.form-modal {
+  width: 80%;
+  display: flex;
+  margin: auto;
+  padding: 2rem;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 1rem;
+  background-color: #c0992f;
+  border: solid 1px #3d3d3d;
+  border-radius: 2vmin;
+  box-shadow: 0 0 15px 2px#3d3d3d;
+}
+.label-modal {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+}
+
+.form-modal > .btn-modal {
+  opacity: 0.5;
+  pointer-events: none;
+}
+
+.form-modal:valid > .btn-modal {
+  opacity: 1;
+  pointer-events: all;
+}
+
+.input-modal {
+  background-color: transparent;
+  padding: 1vmin 0;
+  border: none;
+  border-radius: 1vmin;
+  font-family: "Press Start 2P", cursive;
+  color: #3d3d3d;
+  font-size: 102%;
+}
+
+.input-modal:focus,
+.input-modal:active {
+  outline: none;
+}
+
+.input-modal::placeholder {
+  color: #3d3d3d;
+  opacity: 1;
+  font-size: 110%;
+}
+
+.input-modal:placeholder-shown {
+  opacity: 1;
+  animation: blink 0.9s ease-in-out infinite;
+}
+
+.btn-modal {
+  padding: 1vmin;
+  background-color: #907323;
+  color: #3d3d3d;
+  border: solid 1px#3d3d3d;
+  border-radius: 1vmin;
+  font-family: "Press Start 2P", cursive;
+  cursor: pointer;
+  align-self: flex-end;
+}
+
+.btn-modal:hover {
+  box-shadow: inset 0 0 0 2px #3d3d3d;
+}
+
+@keyframes blink {
+  0% {
+    opacity: 1;
+  }
+  40% {
+    opacity: 0;
+  }
+  60% {
+    opacity: 1;
+  }
+}
+</style>
